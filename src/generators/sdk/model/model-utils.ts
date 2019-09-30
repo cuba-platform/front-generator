138ea84720aa7951ef0f28f54c1bdbf1fa03b287
Vadim Basko <vadim.p.basko@gmail.com>
2019-10-01 00:12:23 +0400

    add coverage tests

--- src/generators/sdk/model/model-utils.ts
+++ src/generators/sdk/model/model-utils.ts
@@ -1,6 +1,6 @@
 import {ProjectEntityInfo} from "./entities-generation";
 import {EnumDeclaration} from "typescript";
-import {Entity, EntityAttribute, getEntitiesArray, ProjectModel} from "../../../common/model/cuba-model";
+import {Entity, getEntitiesArray, ProjectModel} from "../../../common/model/cuba-model";
 import {createEnums} from "./enums-generation";
 
 export type ModelContext = {
@@ -26,30 +26,4 @@ export function collectModelContext(projectModel: ProjectModel) {
   entities.forEach(addEntityToMap(entitiesMap));
   baseProjectEntities.forEach(addEntityToMap(entitiesMap, true));
   return {entitiesMap, enumsMap}
-}
-
-//todo add assert in integration test with inheritor attrs (sec$User)
-export function collectAttributesFromHierarchy(entity: Entity, projectModel: ProjectModel): EntityAttribute[] {
-  let attrs: EntityAttribute[] = entity.attributes;
-
-  const allEntities: Partial<Entity>[] = ([] as Partial<Entity>[])
-    .concat(projectModel.entities)
-    .concat(projectModel.baseProjectEntities ? projectModel.baseProjectEntities : []);
-
-  let {parentClassName, parentPackage} = entity;
-
-  while (parentClassName && parentPackage && parentClassName.length > 0 && parentPackage.length > 0) {
-    const parentFqn = `${parentPackage}.${parentClassName}`;
-    const parent = allEntities.find(e => e.fqn === parentFqn);
-    if (parent) {
-      attrs = parent.attributes ? attrs.concat(parent.attributes) : attrs;
-      parentPackage = parent.parentPackage ? parent.parentPackage : '';
-      parentClassName = parent.parentClassName ? parent.parentClassName : '';
-    } else {
-      parentPackage = '';
-      parentClassName = '';
-    }
-  }
-
-  return attrs;
 }
