64878ee70742763b9e5dee2f66c4e6c6b32fb80b
Vadim Basko <vadim.p.basko@gmail.com>
2019-11-01 17:27:29 +0400

    Template processing - ejs templates for react entity management #42

--- src/generators/react-typescript/entity-cards/index.ts
+++ src/generators/react-typescript/entity-cards/index.ts
@@ -29,8 +29,8 @@ class EntityCardsGenerator extends BaseGenerator<EntityCardsAnswers, EntityCards
     }
     this.model = entityCardsAnswersToModel(this.answers, this.options.dirShift);
     this.fs.copyTpl(
-      this.templatePath('EntityCards.tsx'),
-      this.destinationPath(this.model.className + '.tsx'), this.model
+      this.templatePath('EntityCards.tsx.ejs'),
+      this.destinationPath(this.model.className + '.tsx.ejs'), this.model
     );
 
     if (!addToMenu(this.fs, {
